7964f01faf7247d60414be085e345512d5c5fcac
keerthiram2612 <keerthiram2612@gmail.com>
2023-05-05 06:04:22 +0530

    updated the file

---
+++ multiplication-app/index.js
@@ -0,0 +1,42 @@
+const num1 = Math.ceil(Math.random()*10)
+const num2 = Math.ceil(Math.random()*10)
+
+const questionE1 = document.getElementById("question");
+
+const inputE1 = document.getElementById("input");
+
+const formE1 = document.getElementById("form");
+
+const scoreE1 = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+if(!score){
+
+    score = 0;
+
+}
+
+scoreE1.innerText = `score: ${score}`
+
+questionE1.innerText = `what is ${num1} multiply by ${num2}?`
+
+const correctAns = num1 * num2;
+
+formE1.addEventListener("submit", ()=>{
+    const userAns = +inputE1.value;  
+    if(userAns===correctAns){
+        score++;
+        console.log(score);
+        updateLocalStorage()
+    }
+        else{
+            score--;
+            console.log(score);
+            updateLocalStorage()
+        }
+
+});
+
+function updateLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score))
+}
