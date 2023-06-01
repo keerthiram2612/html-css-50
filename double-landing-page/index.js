6c9c1bff71bc2f2931aa973c7eed24b5bbc39ffa
keerthiram2612 <keerthiram2612@gmail.com>
2023-06-01 06:08:25 +0530

    using event handling of validations

--- double-landing-page/index.js
+++ double-landing-page/index.js
@@ -5,5 +5,14 @@ const leftE1 = document.querySelector(".left");
 const rightE1 = document.querySelector(".right");
 
 leftE1.addEventListener("mouseenter",()=>{
-    containerE1.classList.add("active-left")
+    containerE1.classList.add("active-left");
+})
+leftE1.addEventListener("mouseleave",()=>{
+    containerE1.classList.remove("active-left");
+})
+rightE1.addEventListener("mouseenter",()=>{
+    containerE1.classList.add("active-right");
+})
+rightE1.addEventListener("mouseleave",()=>{
+    containerE1.classList.remove("active-right");
 })
