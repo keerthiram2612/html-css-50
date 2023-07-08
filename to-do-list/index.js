7ab57cf408264e02d540e9f8715c5480633e2bf0
keerthiram2612 <keerthiram2612@gmail.com>
2023-07-08 22:54:42 +0530

    updating the file

---
+++ to-do-list/index.js
@@ -0,0 +1,27 @@
+const formE1 = document.querySelector(".form");
+
+const inputE1 = document.querySelector(".input");
+
+const ulE1 = document.querySelector(".list")
+
+formE1.addEventListener("submit",(event)=>{
+    event.preventDefault();/*Page could not be refresh*/
+    toDoList()
+});
+function toDoList(){
+    let newTask = inputE1.value;
+    const liE1 = document.createElement("li");  
+    liE1.innerText = newTask; 
+    ulE1.appendChild(liE1) 
+    inputE1.value = ""
+    const checkBtnE1 = document.createElement("div")
+    checkBtnE1.innerHTML = `
+    <i class="fa-solid fa-square-check">
+    `
+    liE1.appendChild(checkBtnE1);
+    const  trashE1 = document.createElement("div")
+    trashE1.innerHTML = `
+    <i class="fa-solid fa-trash">
+    `
+    liE1.appendChild(trashE1);
+}
