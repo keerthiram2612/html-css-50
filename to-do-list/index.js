efc982cc9345b281c31375fba71584bc549c24de
keerthiram2612 <keerthiram2612@gmail.com>
2023-07-09 09:24:18 +0530

    creating the localstorage for the list

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -2,15 +2,27 @@ const formE1 = document.querySelector(".form");
 
 const inputE1 = document.querySelector(".input");
 
-const ulE1 = document.querySelector(".list")
+const ulE1 = document.querySelector(".list");
 
+let list = JSON.parse( localStorage.getItem("list"));
+
+list.forEach(task=>{
+    toDoList(task)
+})
 formE1.addEventListener("submit",(event)=>{
     event.preventDefault();/*Page could not be refresh*/
     toDoList()
 });
-function toDoList(){
+function toDoList(task){
     let newTask = inputE1.value;
+    if(task){
+        newTask = task.name
+    }
+    
     const liE1 = document.createElement("li");  
+    if(task && task.checked){
+         liE1.classList.add("checked");
+    }
     liE1.innerText = newTask; 
     ulE1.appendChild(liE1) 
     inputE1.value = ""
@@ -27,5 +39,22 @@ function toDoList(){
 
     checkBtnE1.addEventListener("click",()=>{
         liE1.classList.toggle("checked")
+        updateLocalStorage();
     })
+    trashE1.addEventListener("click",()=>{
+        liE1.remove();
+        updateLocalStorage();
+    })
+    updateLocalStorage();
+}
+function updateLocalStorage(){
+    const liEls = document.querySelectorAll("li");
+     list = []
+    liEls.forEach(liE1=>{
+        list.push({
+            name: liE1.innerText,
+            checked: liE1.classList.contains("checked")
+        })
+    })
+    localStorage.setItem("list",JSON.stringify(list))
 }
